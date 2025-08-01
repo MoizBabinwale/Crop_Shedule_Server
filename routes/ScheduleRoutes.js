86af6e8579937242d36693af0fda266d8485a099
Moiz Babinwale <mbabinwale@gmail.com>
2025-08-01 09:19:27 +0530

    schedule changes

--- routes/ScheduleRoutes.js
+++ routes/ScheduleRoutes.js
@@ -4,11 +4,20 @@ const Schedule = require("../models/Schedule");
 // import Crop from "../models/Crop.js";
 
 //create Shedule
-router.post("/create", async (req, res) => {
+router.post("/create/:cropId", async (req, res) => {
   try {
-    const newSchedule = new Schedule(req.body);
-    const saved = await newSchedule.save();
-    res.status(201).json(saved);
+    const cropId = req.params.cropId;
+
+    const schedules = req.body;
+
+    // Each schedule will be stored with cropId
+    const savedSchedules = await Promise.all(
+      schedules.map((data) => {
+        return new Schedule({ ...data, cropId }).save();
+      })
+    );
+
+    res.status(201).json(savedSchedules);
   } catch (error) {
     console.error("Error saving schedule:", error);
     res.status(500).json({ message: "Failed to save schedule", error });
